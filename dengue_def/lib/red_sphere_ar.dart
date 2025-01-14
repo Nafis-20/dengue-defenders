417ac15337b6e1f79083f8ca2a4531f5b460bf94
Nafis Abdullah <abdullahnafis20@gmail.com>
2025-01-14 21:42:09 +0800

    new

--- dengue_def/lib/red_sphere_ar.dart
+++ dengue_def/lib/red_sphere_ar.dart
@@ -1,32 +1,20 @@
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math_64.dart' as vector;
-import 'package:ar_flutter_plugin_updated/ar_flutter_plugin.dart';
-import 'package:ar_flutter_plugin_updated/datatypes/config_planedetection.dart';
-import 'package:ar_flutter_plugin_updated/datatypes/hittest_result_types.dart';
-import 'package:ar_flutter_plugin_updated/datatypes/node_types.dart';
-import 'package:ar_flutter_plugin_updated/managers/ar_anchor_manager.dart';
-import 'package:ar_flutter_plugin_updated/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin_updated/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin_updated/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin_updated/models/ar_anchor.dart';
-import 'package:ar_flutter_plugin_updated/models/ar_hittest_result.dart';
-import 'package:ar_flutter_plugin_updated/models/ar_node.dart';
-import 'package:ar_flutter_plugin_updated/widgets/ar_view.dart';
+import 'package:ar_flutter_plugin_flutterflow/ar_flutter_plugin_flutterflow.dart';
+import 'package:vector_math/vector_math_64.dart';
 
-class ARPage extends StatefulWidget {
+class ARSpherePage extends StatefulWidget {
   @override
-  _ARPageState createState() => _ARPageState();
+  _ARSpherePageState createState() => _ARSpherePageState();
 }
 
-class _ARPageState extends State<ARPage> {
-  ARSessionManager? arSessionManager;
-  ARObjectManager? arObjectManager;
+class _ARSpherePageState extends State<ARSpherePage> {
+  late ARSessionManager arSessionManager;
+  late ARObjectManager arObjectManager;
 
   @override
   void dispose() {
     super.dispose();
-    arSessionManager?.dispose();
+    arSessionManager.dispose();
   }
 
   @override
@@ -46,31 +34,27 @@ class _ARPageState extends State<ARPage> {
     arSessionManager = sessionManager;
     arObjectManager = objectManager;
 
-    // Initialize AR session and object manager
-    arSessionManager?.onInitialize(
+    // Initialize AR session
+    arSessionManager.onInitialize(
       showFeature: true,
       handleTaps: true,
     );
-    arObjectManager?.onInitialize();
 
-    // Add a 3D model (a red sphere)
+    // Create and add a red sphere
     _addRedSphere();
   }
 
   void _addRedSphere() {
-    // Create a new AR node with a red sphere
-    final sphereNode = ARNode(
-      type:
-          NodeType.webGL, // Specify the node type (WebGL for models from a URL)
-      uri:
-          'https://example.com/3dmodels/red_sphere.glb', // URL to your 3D model (can be local asset or a URL)
-      position: Vector3(0, 0, -1), // Position the model in front of the user
-      scale: Vector3(0.2, 0.2, 0.2), // Scale the model to a reasonable size
-      rotation:
-          Vector4(0, 0, 0, 0), // Optional: Set the rotation (Quaternion format)
+    // Create a 3D node for the red sphere
+    final redSphereNode = ARNode(
+      type: NodeType.local, // Local asset (not web-based)
+      uri: 'assets/red_sphere.obj', // Add your red sphere model in assets
+      position:
+          Vector3(0, 0, -1), // Position the model 1 meter away from the user
+      scale: Vector3(0.2, 0.2, 0.2), // Scale the sphere to a reasonable size
     );
 
-    // Add the node to the AR session
-    arObjectManager?.addNode(sphereNode);
+    // Add the node to the AR scene
+    arObjectManager.addNode(redSphereNode);
   }
 }
