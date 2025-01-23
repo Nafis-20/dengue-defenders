ce75e0834b14e80b73a6980aeef1fd4db3c671d1
Nafis Abdullah <abdullahnafis20@gmail.com>
2025-01-23 14:42:02 +0800

    s

--- dengue_def/lib/red_sphere_ar.dart
+++ dengue_def/lib/red_sphere_ar.dart
@@ -1,48 +1,28 @@
-// import 'package:flutter/material.dart';
-// import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-// import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-// class RedSphereARPage extends StatefulWidget {
-//   const RedSphereARPage({super.key});
+class ARScreen extends StatefulWidget {
+  @override
+  _ARScreenState createState() => _ARScreenState();
+}
 
-//   @override
-//   _RedSphereARPageState createState() => _RedSphereARPageState();
-// }
+class _ARScreenState extends State<ARScreen> {
+  late UnityWidgetController _unityWidgetController;
 
-// class _RedSphereARPageState extends State<RedSphereARPage> {
-//   late ArCoreController _arCoreController;
+  void onUnityCreated(UnityWidgetController controller) {
+    _unityWidgetController = controller;
+  }
 
-//   @override
-//   void dispose() {
-//     _arCoreController.dispose();
-//     super.dispose();
-//   }
-
-//   void _onArCoreViewCreated(ArCoreController controller) {
-//     _arCoreController = controller;
-
-//     // Add a red sphere to the AR scene
-//     final material = ArCoreMaterial(color: Colors.red);
-//     final sphere =
-//         ArCoreSphere(materials: [material], radius: 0.1); // Radius in meters
-//     final node = ArCoreNode(
-//       shape: sphere,
-//       position: vector.Vector3(0, 0, -1), // 1 meter in front of the camera
-//     );
-
-//     _arCoreController.addArCoreNode(node);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Red Sphere AR'),
-//       ),
-//       body: ArCoreView(
-//         onArCoreViewCreated: _onArCoreViewCreated,
-//         enableTapRecognizer: true,
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AR Unity Integration'),
+      ),
+      body: UnityWidget(
+        onUnityCreated: onUnityCreated,
+        fullscreen: true,
+      ),
+    );
+  }
+}
