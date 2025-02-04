06681b65a2a1cbd55e59e1182aec51fb8536e301
Nafis Abdullah <abdullahnafis20@gmail.com>
2025-02-05 00:34:32 +0600

    l

--- dengue_def/lib/main.dart
+++ dengue_def/lib/main.dart
@@ -1,23 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'auth/landing_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'auth/landing_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Dengue Defender',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: const LandingPage(),
+//     );
+//   }
+// }
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dengue Defender',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LandingPage(),
+      home: UnityARScreen(),
+    );
+  }
+}
+
+class UnityARScreen extends StatefulWidget {
+  @override
+  _UnityARScreenState createState() => _UnityARScreenState();
+}
+
+class _UnityARScreenState extends State<UnityARScreen> {
+  UnityWidgetController? _unityWidgetController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Unity AR in Flutter")),
+      body: UnityWidget(
+        onUnityCreated: (controller) {
+          _unityWidgetController = controller;
+        },
+      ),
     );
   }
 }
